b5c9815e6bf90b9a080103354bb3ccce7da4bbd5
ahmdalmas <ahmdalmasbsns@gmail.com>
2023-09-28 20:47:27 +0530

    theme default dark setted

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,52 +39,62 @@
   /*===================================
    Dark and light theme change
   =====================================*/
-  var themes = [{
+  // Define your themes
+var themes = [
+  {
     name: "dark",
     "class": "dark",
     checked: false
-  }, {
+  },
+  {
     name: "semiDark",
     "class": "semiDark",
     checked: false
-  }, {
+  },
+  {
     name: "light",
     "class": "light",
     checked: false
-  }];
+  }
+];
 
-  // Loop through themes and add event listener for changes
-  themes.forEach(function (theme) {
-    var radioBtn = $("#".concat(theme["class"]));
-    radioBtn.prop("checked", theme.name === currentTheme);
-    radioBtn.on("change", function () {
-      if (this.checked) {
-        currentTheme = theme.name;
-        localStorage.theme = theme.name;
-        location.reload();
-      }
-    });
-  });
+// Set the default theme to "dark"
+var currentTheme = "dark";
 
-  // Theme Change by Header Button
-  $("#themeMood").on("click", function () {
-    if (currentTheme === "dark") {
-      currentTheme = "light";
-    } else {
-      currentTheme = "dark";
-    }
-    localStorage.theme = currentTheme;
-    location.reload();
-  });
-  $("#grayScale").on("click", function () {
-    if ($("html").hasClass("grayScale")) {
-      $("html").removeClass("grayScale");
-      localStorage.effect = "";
-    } else {
-      $("html").addClass("grayScale");
-      localStorage.effect = "grayScale";
+// Loop through themes and add event listener for changes
+themes.forEach(function (theme) {
+  var radioBtn = $("#" + theme["class"]);
+  radioBtn.prop("checked", theme.name === currentTheme);
+  radioBtn.on("change", function () {
+    if (this.checked) {
+      currentTheme = theme.name;
+      localStorage.theme = theme.name;
+      location.reload();
     }
   });
+});
+
+// Theme Change by Header Button
+$("#themeMood").on("click", function () {
+  if (currentTheme === "dark") {
+    currentTheme = "light";
+  } else {
+    currentTheme = "dark";
+  }
+  localStorage.theme = currentTheme;
+  location.reload();
+});
+
+$("#grayScale").on("click", function () {
+  if ($("html").hasClass("grayScale")) {
+    $("html").removeClass("grayScale");
+    localStorage.effect = "";
+  } else {
+    $("html").addClass("grayScale");
+    localStorage.effect = "grayScale";
+  }
+});
+
 
   /*===================================
    Layout Changer
